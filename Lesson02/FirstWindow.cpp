c3166e2920e4e7418a73dfc1d247a371316b1734
Michael Patterson <mjpatter88@gmail.com>
2015-09-22 12:53:23 -0500

    Actually completed code for Lesson02.

--- Lesson02/FirstWindow.cpp
+++ Lesson02/FirstWindow.cpp
@@ -4,29 +4,72 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+bool init();
+bool loadMedia();
+void close();
+
+// Surface rendering is slow and done through software.
+SDL_Window *window = NULL;
+SDL_Surface *screenSurface = NULL;
+SDL_Surface *imageSurface = NULL;
+
 int main(int argc, char* argv[])
 {
-    SDL_Window *window;
-    SDL_Surface *surface;
-
-    if(SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    }
-    else {
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if(window == NULL) {
-            printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
-        }
-        else {
-            surface = SDL_GetWindowSurface(window);
-            SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(2000);
-        }
-		SDL_DestroyWindow(window);
-    }
-
-    
-    SDL_Quit();
+	if (init() != true) {
+		printf("Failed to initialize.\n");
+	}
+	else {
+		if (loadMedia() != true) {
+			printf("Failed to load media.\n");
+		}
+		else {
+			SDL_BlitSurface(imageSurface, NULL, screenSurface, NULL);
+			SDL_UpdateWindowSurface(window);
+			SDL_Delay(2000);
+		}
+	}
+	close();
     return 0;
+}
+
+bool init()
+{
+	bool success = true;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize: %s\n", SDL_GetError());
+		success = false;
+	}
+	else {
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL) {
+			printf("Window could not be created: %s\n", SDL_GetError());
+			success = false;
+		}
+		else {
+			screenSurface = SDL_GetWindowSurface(window);
+		}
+	}
+	return success;
+}
+
+bool loadMedia()
+{
+	bool success = true;
+	imageSurface = SDL_LoadBMP("guest.bmp");
+	if (imageSurface == NULL) {
+		printf("Unable to load image %s. Error: %s\n", "guest.bmp", SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	SDL_FreeSurface(imageSurface);
+	imageSurface = NULL;
+
+	SDL_DestroyWindow(window);	//Also destroys the screen surface.
+	window = NULL;
+
+	SDL_Quit();
 }
